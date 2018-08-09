400a3e0ca1a24eb0cd1d7d9214e06f3f264346a0
Tomasz Jakub Rup <tomasz.rup@gmail.com>
2018-08-09 11:41:52 +0200

    mirror from github.com/tomi77/go-passwd; extract hasher

--- hasher.go
+++ hasher.go
@@ -1,11 +1,9 @@
-package passwd
+package hasher
 
 import (
 	"fmt"
 	"strconv"
 	"strings"
-
-	"github.com/tomi77/go-passwd/passwd/hasher"
 )
 
 // Hasher interface
@@ -26,77 +24,77 @@ type Hasher interface {
 func NewHasherFromString(password string) (Hasher, error) {
 	p := strings.Split(password, "$")
 	switch p[0] {
-	case hasher.PlainHasher{}.Code():
-		return &hasher.PlainHasher{Password: &p[1]}, nil
-	case hasher.MD5Hasher{}.Code():
+	case PlainHasher{}.Code():
+		return &PlainHasher{Password: &p[1]}, nil
+	case MD5Hasher{}.Code():
 		iter, _ := strconv.Atoi(p[1])
-		return &hasher.MD5Hasher{Salt: &p[2], Iter: &iter, Password: &p[3]}, nil
-	case hasher.SHA1Hasher{}.Code():
+		return &MD5Hasher{Salt: &p[2], Iter: &iter, Password: &p[3]}, nil
+	case SHA1Hasher{}.Code():
 		iter, _ := strconv.Atoi(p[1])
-		return &hasher.SHA1Hasher{Salt: &p[2], Iter: &iter, Password: &p[3]}, nil
-	case hasher.SHA224Hasher{}.Code():
+		return &SHA1Hasher{Salt: &p[2], Iter: &iter, Password: &p[3]}, nil
+	case SHA224Hasher{}.Code():
 		iter, _ := strconv.Atoi(p[1])
-		return &hasher.SHA224Hasher{Salt: &p[2], Iter: &iter, Password: &p[3]}, nil
-	case hasher.SHA256Hasher{}.Code():
+		return &SHA224Hasher{Salt: &p[2], Iter: &iter, Password: &p[3]}, nil
+	case SHA256Hasher{}.Code():
 		iter, _ := strconv.Atoi(p[1])
-		return &hasher.SHA256Hasher{Salt: &p[2], Iter: &iter, Password: &p[3]}, nil
-	case hasher.SHA384Hasher{}.Code():
+		return &SHA256Hasher{Salt: &p[2], Iter: &iter, Password: &p[3]}, nil
+	case SHA384Hasher{}.Code():
 		iter, _ := strconv.Atoi(p[1])
-		return &hasher.SHA384Hasher{Salt: &p[2], Iter: &iter, Password: &p[3]}, nil
-	case hasher.SHA512Hasher{}.Code():
+		return &SHA384Hasher{Salt: &p[2], Iter: &iter, Password: &p[3]}, nil
+	case SHA512Hasher{}.Code():
 		iter, _ := strconv.Atoi(p[1])
-		return &hasher.SHA512Hasher{Salt: &p[2], Iter: &iter, Password: &p[3]}, nil
-	case hasher.SHA512_224Hasher{}.Code():
+		return &SHA512Hasher{Salt: &p[2], Iter: &iter, Password: &p[3]}, nil
+	case SHA512_224Hasher{}.Code():
 		iter, _ := strconv.Atoi(p[1])
-		return &hasher.SHA512_224Hasher{Salt: &p[2], Iter: &iter, Password: &p[3]}, nil
-	case hasher.SHA512_256Hasher{}.Code():
+		return &SHA512_224Hasher{Salt: &p[2], Iter: &iter, Password: &p[3]}, nil
+	case SHA512_256Hasher{}.Code():
 		iter, _ := strconv.Atoi(p[1])
-		return &hasher.SHA512_256Hasher{Salt: &p[2], Iter: &iter, Password: &p[3]}, nil
+		return &SHA512_256Hasher{Salt: &p[2], Iter: &iter, Password: &p[3]}, nil
 	}
 	return nil, fmt.Errorf("Unsupported hasher %s", p[0])
 }
 
 // NewSHA512_256Hasher returns new default SHA-512/256 hasher
 func NewSHA512_256Hasher() Hasher {
-	return &hasher.SHA512_256Hasher{}
+	return &SHA512_256Hasher{}
 }
 
 // NewSHA512_224Hasher returns new default SHA-512/224 hasher
 func NewSHA512_224Hasher() Hasher {
-	return &hasher.SHA512_224Hasher{}
+	return &SHA512_224Hasher{}
 }
 
 // NewSHA512Hasher returns new default SHA-512 hasher
 func NewSHA512Hasher() Hasher {
-	return &hasher.SHA512Hasher{}
+	return &SHA512Hasher{}
 }
 
 // NewSHA384Hasher returns new default SHA-384 hasher
 func NewSHA384Hasher() Hasher {
-	return &hasher.SHA384Hasher{}
+	return &SHA384Hasher{}
 }
 
 // NewSHA256Hasher returns new default SHA-256 hasher
 func NewSHA256Hasher() Hasher {
-	return &hasher.SHA256Hasher{}
+	return &SHA256Hasher{}
 }
 
 // NewSHA224Hasher returns new default SHA-224 hasher
 func NewSHA224Hasher() Hasher {
-	return &hasher.SHA224Hasher{}
+	return &SHA224Hasher{}
 }
 
 // NewSHA1Hasher returns new default SHA-224 hasher
 func NewSHA1Hasher() Hasher {
-	return &hasher.SHA1Hasher{}
+	return &SHA1Hasher{}
 }
 
 // NewMD5Hasher returns new default MD5 hasher
 func NewMD5Hasher() Hasher {
-	return &hasher.MD5Hasher{}
+	return &MD5Hasher{}
 }
 
 // NewPlainHasher returns new default plain hasher
 func NewPlainHasher() Hasher {
-	return &hasher.PlainHasher{}
+	return &PlainHasher{}
 }

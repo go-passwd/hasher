c35f6d13f764be4702ed13a22d5b156e51daee8a
Tomasz Jakub Rup <tomasz.rup@gmail.com>
2019-07-14 19:46:01 +0200

    moved to marshaler repo

--- predefined_marshalers.go
+++
@@ -1,6 +0,0 @@
-package hasher
-
-// DjangoMarshaler stores passwords in Django like format
-var DjangoMarshaler = HexMarshaler{Separator: "$"}
-
-// var DjangoMarshaler = HexMarshaler{Template: "{{.Code}}${{.Iterations}}${{.Salt}}${{.Password}}", Pattern: "^(\\w+)\\$(\\d+)\\$(\\w*)\\$(\\w+)$"}

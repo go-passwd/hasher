acd6202182043dc86b249f506708e428de8772b5
Tomasz Jakub Rup <tomasz.rup@gmail.com>
2019-07-15 11:38:52 +0200

    fixed tests (again)

--- hasher_test.go
+++ hasher_test.go
@@ -9,43 +9,43 @@ import (
 var strPassword = "plain$password"
 
 func TestNew(t *testing.T) {
-	g, e := New(TypePlain, nil, nil, nil)
+	g, e := New(TypePlain)
 	assert.Nil(t, e)
 	assert.IsType(t, &PlainHasher{}, g)
 
-	g, e = New(TypeMD5, nil, nil, nil)
+	g, e = New(TypeMD5)
 	assert.Nil(t, e)
 	assert.IsType(t, &MD5Hasher{}, g)
 
-	g, e = New(TypeSHA1, nil, nil, nil)
+	g, e = New(TypeSHA1)
 	assert.Nil(t, e)
 	assert.IsType(t, &SHA1Hasher{}, g)
 
-	g, e = New(TypeSHA224, nil, nil, nil)
+	g, e = New(TypeSHA224)
 	assert.Nil(t, e)
 	assert.IsType(t, &SHA224Hasher{}, g)
 
-	g, e = New(TypeSHA256, nil, nil, nil)
+	g, e = New(TypeSHA256)
 	assert.Nil(t, e)
 	assert.IsType(t, &SHA256Hasher{}, g)
 
-	g, e = New(TypeSHA384, nil, nil, nil)
+	g, e = New(TypeSHA384)
 	assert.Nil(t, e)
 	assert.IsType(t, &SHA384Hasher{}, g)
 
-	g, e = New(TypeSHA512, nil, nil, nil)
+	g, e = New(TypeSHA512)
 	assert.Nil(t, e)
 	assert.IsType(t, &SHA512Hasher{}, g)
 
-	g, e = New(TypeSHA512_224, nil, nil, nil)
+	g, e = New(TypeSHA512_224)
 	assert.Nil(t, e)
 	assert.IsType(t, &SHA512_224Hasher{}, g)
 
-	g, e = New(TypeSHA512_256, nil, nil, nil)
+	g, e = New(TypeSHA512_256)
 	assert.Nil(t, e)
 	assert.IsType(t, &SHA512_256Hasher{}, g)
 
-	g, e = New("unsupported", nil, nil, nil)
+	g, e = New("unsupported")
 	assert.Nil(t, g)
 	assert.Errorf(t, e, "Unsupported hasher unsupported")
 }
